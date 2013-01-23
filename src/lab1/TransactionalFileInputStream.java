a67c91a2f184fdaf055c763cb18785e1fa2f7da9
wangzhenhuaCode <wangzhenhua.mail@gmail.com>
2013-01-22 21:37:00 -0500

    fin

--- src/lab1/TransactionalFileInputStream.java
+++ src/lab1/TransactionalFileInputStream.java
@@ -1,5 +1,7 @@
 package lab1;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
@@ -10,18 +12,30 @@ public class TransactionalFileInputStream extends InputStream implements Seriali
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 7779242789718779893L;
+	private static final long serialVersionUID = 1L;
 	private String fileName;
-	public TransactionalFileInputStream(String fileName) {
+	private Integer readSize;
+	private FileInputStream fin;
+	public TransactionalFileInputStream(String fileName,Integer readSize) {
 		super();
 		this.fileName = fileName;
+		//readSize=0;
+		this.readSize=readSize;
+		try {
+			fin=new FileInputStream(fileName);
+			fin.skip(readSize);
+		}catch(IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public int read() throws IOException {
 		// TODO Auto-generated method stub
-		
-		return 0;
+		readSize++;
+		System.out.println(readSize);
+		return fin.read();
 	}
 
 }
