ecfef78eb45fc7a285f0e45c09a5e432baf32c05
wangzhenhuaCode <wangzhenhua.mail@gmail.com>
2013-01-22 21:12:08 -0500

    d

--- src/lab1/TransactionalFileInputStream.java
+++ src/lab1/TransactionalFileInputStream.java
@@ -4,10 +4,15 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class TransactionalFileInputStream extends InputStream {
-
+	private String fileName;
+	public TransactionalFileInputStream(String fileName) {
+		super();
+		this.fileName = fileName;
+	}
 	@Override
 	public int read() throws IOException {
 		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
