63bf0740aee80967725b3d8585844f2711d295c6
wangzhenhuaCode <wangzhenhua.mail@gmail.com>
2013-01-22 21:43:56 -0500

    ds

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,5 +1,8 @@
 package lab1;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+
 public class Main {
 
 	/**
@@ -7,7 +10,18 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		TransactionalFileInputStream in=new TransactionalFileInputStream("text.txt",0);
+		DataInputStream is = new DataInputStream(in);
+		try {
+			int b=is.read();
+			while(b!=-1){
+				System.out.println(b);
+				b=is.read();
+			};
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
