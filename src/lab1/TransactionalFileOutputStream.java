f7acebe7d5b6cb90de7c515b1f8959da5d57c282
wangzhenhuaCode <wangzhenhua.mail@gmail.com>
2013-01-22 20:56:32 -0500

    io

--- src/lab1/TransactionalFileOutputStream.java
+++ src/lab1/TransactionalFileOutputStream.java
@@ -1,5 +1,14 @@
 package lab1;
 
-public class TransactionalFileOutputStream {
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class TransactionalFileOutputStream extends OutputStream {
+
+	@Override
+	public void write(int arg0) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
