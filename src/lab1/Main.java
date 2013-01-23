2c5a67762857142de3907715ceef95ecb89f2153
wangzhenhuaCode <wangzhenhua.mail@gmail.com>
2013-01-22 22:24:50 -0500

    d

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,5 +1,7 @@
 package lab1;
 
+
+
 public class Main {
 
 	/**
@@ -7,7 +9,23 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		byte[] buffer=new byte[1024];
+		int n=0;
+		while(true){
+			try{
+			n=System.in.read(buffer);
+			String command=new String(buffer,0,n-1);
+			if(command.equals("quit")){
+				System.out.println("Bye!");
+				break;
+			}else{
+				System.out.println(command);
+			}
+				
+			}catch(Exception e){
+				
+			}
+		}
 	}
 
 }
