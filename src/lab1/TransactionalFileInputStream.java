1afbd854980e8775ad58c1c97c0dec99f78498f2
GameOwen <gameowencz@gmail.com>
2013-01-22 21:08:40 -0500

    s

--- src/lab1/TransactionalFileInputStream.java
+++ src/lab1/TransactionalFileInputStream.java
@@ -2,8 +2,9 @@ package lab1;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Serializable;
 
-public class TransactionalFileInputStream extends InputStream {
+public class TransactionalFileInputStream extends InputStream implements Serializable {
 
 	@Override
 	public int read() throws IOException {
--- src/lab1/TransactionalFileOutputStream.java
+++ src/lab1/TransactionalFileOutputStream.java
@@ -2,8 +2,9 @@ package lab1;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.Serializable;
 
-public class TransactionalFileOutputStream extends OutputStream {
+public class TransactionalFileOutputStream extends OutputStream implements Serializable{
 
 	@Override
 	public void write(int arg0) throws IOException {
