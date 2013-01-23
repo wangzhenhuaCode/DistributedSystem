64d2c923cd6c80f5a074ec76aa7b8225db48f486
GameOwen <gameowencz@gmail.com>
2013-01-22 21:18:36 -0500

    s

--- src/lab1/TransactionalFileOutputStream.java
+++ src/lab1/TransactionalFileOutputStream.java
@@ -5,12 +5,17 @@ import java.io.OutputStream;
 import java.io.Serializable;
 
 public class TransactionalFileOutputStream extends OutputStream implements Serializable{
-
-	/**
-	 * 
-	 */
+	private String filename;
+	private boolean result;
+	
 	private static final long serialVersionUID = -7131932025690436925L;
 
+	
+	public TransactionalFileOutputStream(String filename, boolean result){
+		super();
+		this.filename = filename;
+		this.result = result;
+	}
 	@Override
 	public void write(int arg0) throws IOException {
 		// TODO Auto-generated method stub
